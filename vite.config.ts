f6a52d8fb60b3a6a6e01a01e8bb07199b9d40825
John Hawley <jehawley@gmail.com>
2025-08-21 04:26:44 -0700

    commit 17

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
-import build from '@hono/vite-build/cloudflare-pages'
+import build from '@hono/vite-build'
 import devServer from '@hono/vite-dev-server'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      build: {
+        rollupOptions: {
+          input: './src/renderer.tsx',
+          output: {
+            entryFileNames: 'static/client.js'
+          }
+        }
+      }
+    }
+  } else {
+    return {
+      plugins: [
+        build({
+          entry: 'src/index.tsx'
+        }),
+        devServer({
+          adapter,
+          entry: 'src/index.tsx'
+        })
+      ]
+    }
+  }
 })
